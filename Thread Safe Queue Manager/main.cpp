5eb5b809691be7927ca01473b8a1cbe29f4bb262
Tim Madorma <tmadorma@gmail.com>
2018-01-03 00:30:22 -0500

    Initial Commit

---
+++ Thread Safe Queue Manager/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Thread Safe Queue Manager
+//
+//  Created by Tim Madorma on 1/3/18.
+//  Copyright © 2018 Tim Madorma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
